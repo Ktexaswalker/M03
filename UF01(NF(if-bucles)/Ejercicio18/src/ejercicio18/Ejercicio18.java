cf671f13f2cee9429981bbcf9121c01c0f9d566b
Hector Martinez Cervera <hema5364@proven.local>
2023-10-02 15:01:43 +0200

    Ejercicio18

---
+++ UF01(NF(if-bucles)/Ejercicio18/src/ejercicio18/Ejercicio18.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio18;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hema5364
+ */
+public class Ejercicio18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float extra;
+        float dinero;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Cuantas horas has trabajado? ");
+        int horas = sc.nextInt();
+        if (horas > 35) {
+            extra = 1.5 * (float) (horas - 35);
+        } else {
+            extra = 0;
+        }
+        horas = extra + horas;
+        dinero = 20 * horas;
+        System.out.println("Cobraras: " + dinero);
+    }
+    
+}
